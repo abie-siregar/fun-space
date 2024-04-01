3c64d0e4d7393f5dc0636ae62b51ec9fc028c9d7
abie-siregar <habibullahsiregar17@gmail.com>
2024-04-01 08:09:39 +0700

    First Github Project

---
+++ app.py
@@ -0,0 +1,7 @@
+def luas_segitiga():
+    a = int(input("Masukkan Alasa Segitiga : "))
+    t = int(input("Masukkan tinggi Segitiga : "))
+    luas = a * t / 2 
+    print("Luas Segitiga adalah",luas)
+
+luas_segitiga()
